f34f54023be0d6923f3e39aa60ec0b7aede1baa8
Victor Mikell <78280727+vmikell@users.noreply.github.com>
2021-08-09 00:46:15 -0400

    Add files via upload

---
+++ build/precache-manifest.2a7f2670000bdbbbcfde2ea5d84bef46.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceaad3bf3f867ef2a90d1a2ab6a8c552",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ce5623d6d72efe5048c",
+    "url": "/static/css/main.58f7bae1.chunk.css"
+  },
+  {
+    "revision": "a536404d0fa57849714a",
+    "url": "/static/js/2.2ff22ec8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.2ff22ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ce5623d6d72efe5048c",
+    "url": "/static/js/main.23fc4a2b.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  },
+  {
+    "revision": "9d561516bb85df2c3d4f6e4101f71cf2",
+    "url": "/static/media/kghappy-webfont.9d561516.woff"
+  },
+  {
+    "revision": "c040cdc4cfe379b3f01802a829c829ea",
+    "url": "/static/media/kghappy-webfont.c040cdc4.woff2"
+  },
+  {
+    "revision": "2e48eada9882444536e8eb137f42b5b5",
+    "url": "/static/media/kghappysolid-webfont.2e48eada.woff"
+  },
+  {
+    "revision": "960ca525958bcc3c62a9a53071679f63",
+    "url": "/static/media/kghappysolid-webfont.960ca525.woff2"
+  },
+  {
+    "revision": "a12c330d772b0e5049cb8c43f331d7f1",
+    "url": "/static/media/kgtangledupinyou-webfont.a12c330d.woff"
+  },
+  {
+    "revision": "e042ffe606bd6a4261a6e7100c75fb4d",
+    "url": "/static/media/kgtangledupinyou-webfont.e042ffe6.woff2"
+  },
+  {
+    "revision": "47e0d1b0f5f83f8ed4498ffda2b66c52",
+    "url": "/static/media/kgtangledupinyou2-webfont.47e0d1b0.woff"
+  },
+  {
+    "revision": "d1bad44b7b02fb426cdf79aae5b492c3",
+    "url": "/static/media/kgtangledupinyou2-webfont.d1bad44b.woff2"
+  }
+]);
